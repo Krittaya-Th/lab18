068db698418baeae99b7d4fc5208edda25c8c363
Krittaya-Th <krittayaclf9028@gmail.com>
2026-02-13 18:06:31 +0700

    lab18_2_completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,46 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect *R1,Rect *R2){
+        double A,B,C,D,E,F,G,H,I,J,K,L;
+        if((R2->x+R2->w)-(R1->x+R1->w)>=R2->w||(R1->x+R1->w)-(R2->x+R2->w)>=R1->w){
+            return 0;
+        }else if((R2->y+R2->h)-(R1->y+R1->h)>=R2->h||(R1->y+R1->h)-(R2->y+R2->h)>=R1->h){
+            return 0;
+        }else{
+            A=R1->x;
+            B=R2->x;
+            if(A>=B){
+                C=A;
+            }else{
+                C=B;
+            }
+            D=A+R1->w;
+            E=B+R2->w;
+            if(D<=E){
+                F=D-C;
+            }else{
+                F=E-C;
+            }
+        
+            G=R1->y;
+            H=R2->y;
+            if(G<=H){
+                I=G;
+            }else{
+                I=H;
+            }
+            J=G-R1->h;
+            K=H-R2->h;
+            if(J>=K){
+                L=I-J;
+            }else{
+                L=I-K;
+            }
+        
+            return F*L;
+        }
+        
+        
 
 }
